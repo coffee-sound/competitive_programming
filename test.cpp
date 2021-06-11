b3ce3f94c64a67eb7a2882bf4c164140c1ce641b
coffee-sound <coffee-sound@users.noreply.github.com>
2021-06-12 05:13:00 +0900

    commpetitive programing library

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using Graph = vector<vector<long long>>
+
+template<class T> void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+
+const long long INF = 1LL << 60;
